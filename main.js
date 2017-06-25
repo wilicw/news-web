e0a9412040fbb12c71b4612f27cb4ddf78509971
William Chang <william20021026@gmail.com>
2017-06-25 19:45:34 +0800

    news

---
+++ main.js
@@ -0,0 +1,49 @@
+
+$(function(){
+    var conf_colors = "uk-card uk-card-default uk-card-hover uk-card-body";
+    load_udn();
+    function load_udn() {
+        $.ajax({ //聯合新聞網
+            url:'https://udn.com/rssfeed/latest?ch=news',
+            type: 'GET',
+            dataType: 'xml',
+            timeout: 5000,
+            error: function(xml){
+                console.log('讀取xml錯誤'+xml);
+            },
+            success: function(xml){
+                $(xml).find("item").each(function(i){
+                    var allnews=$(xml).find("item").length;//xml的總筆數
+    		        var news_title=$(this).children("title").text(); //取得子節點中的資料
+                    var news_url=$(this).children("link").text();
+                    var news_time=$(this).children("pubDate").text();
+    		        var news_description=$(this).children("description").text();
+                    var read_url="readudn.html?src="+btoa(encodeURIComponent(news_url));
+                    $("#index_body").append("<div class=\"" + conf_colors + "\"><article class=\"uk-article\"><h1 class=\"uk-article-title\"><a class=\"uk-link-reset\" href=\"" + read_url + "\">" + news_title + "</a></h1><p class=\"uk-article-meta\">聯合新聞網</p><p class=\"uk-text-meta uk-margin-remove-top\"><time datetime=\"" + news_time + "\">April 01, 2016</time></p><p class=\"uk-text-lead\">" + news_description + "</p><div class=\"uk-grid-small uk-child-width-auto\" uk-grid><div class=\"uk-card-footer\"><a class=\"uk-button uk-button-text\" href=\"" + read_url + "\">閱讀更多...</a></div></div></article></div>");
+                });
+            }
+        });
+    }
+
+    function load_apple() {
+        $.ajax({ //蘋果日報
+            url:'http://www.appledaily.com.tw/rss/newcreate/kind/rnews/type/new',
+            type: 'GET',
+            dataType: 'xml',
+            timeout: 10000,
+            error: function(xml){
+                console.log('讀取xml錯誤'+xml);
+            },
+            success: function(xml){
+                $(xml).find("item").each(function(i){
+                    var allnews=$(xml).find("item").length;//xml的總筆數
+    		        var news_title=$(this).children("title").text(); //取得子節點中的資料
+                    var news_url=$(this).children("link").text();
+    		        var news_description=$(this).children("description").text();
+
+                    $("#index_body").append("<div class=\"" + conf_colors + "\"><article class=\"uk-article\"><h1 class=\"uk-article-title\"><a class=\"uk-link-reset\" href=\"" + news_url + "\">" + news_title + "</a></h1><p class=\"uk-article-meta\">蘋果日報</p><div class=\"uk-grid-small uk-child-width-auto\" uk-grid><div><a class=\"uk-button uk-button-text\" href=\"" + news_url + "\">閱讀更多...</a></div></div></article></div>");
+                });
+            }
+        });
+    }
+});
---
+++ read.js
@@ -0,0 +1,37 @@
+$(function(){
+    var url = window.location.toString();
+    var str = "";
+    if (url.indexOf("?") != -1) {
+	       var ary=url.split("?")[1].split("&");
+	        for (var i in ary){
+		        str += ary[i].split("=")[1] + "\n"; 	//參數值
+	        }
+    }
+    var urls = decodeURIComponent(atob(str));
+    console.log(urls);
+    read_udn()
+    function read_udn() {
+        $.ajax({ //聯合新聞網
+            url:urls,
+            type: 'GET',
+            dataType: 'html',
+            timeout: 5000,
+            error: function(data){
+                console.log('讀取錯誤');
+            },
+            success: function(data){
+                var news1 = data.split("<!-- /#story_bady_info -->",2)[1];
+                var news10 = news1.split("<!-- /#story_body_content -->",2)[0];
+                for (var i = 0; i < 10; i++) {
+                    news10 =  news10.replace("<a href=\"####\" class=\"photo_pop_icon\">分享</a>","");
+                    news10 =  news10.replace("<div class=\"photo_pop\"> <ul> <li class=\"facebook\"><a title=\"facebook\" href=\"#\">facebook</a></li> </ul> </div>","");
+                }
+                var title = data.split("<title>",2)[1].split("</title>",2)[0];
+                var date = data.split("<div class=\"story_bady_info_author\">",2)[1].split("<ul><li class=\"like\">",2)[0];
+                $("#index_body").append("<h1 class=\"uk-heading-line uk-text-center\"><span>" + title + "</span></h1>");
+                $("#index_body").append("<p class=\"uk-article-meta\">" + date + "</p>");
+                $("#index_body").append("<div class=\"uk-container\">" + news10 + "<div>");
+            }
+        });
+    }
+});
